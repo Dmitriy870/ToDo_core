33a1a00828ca254b39947e779f44e36cf05a12d8
Dmitriy <pilevichdima20@gmail.com>
2024-12-24 10:05:15 +0300

    Add common and project app

---
+++ projects/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+from common.models import Base
+
+
+class Project(Base):
+    title = models.CharField(max_length=55)
+    description = models.TextField()
+    status = models.CharField(max_length=255)
+    created_by = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="created_projects"
+    )
+
+    class Meta:
+        db_table = "project"
+
+
+class ProjectUser(Base):
+    ROLE_CHOICES = [
+        ("Reader", "Reader"),
+        ("Developer", "Developer"),
+        ("Maintainer", "Maintainer"),
+        ("Owner", "Owner"),
+    ]
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name="project_users")
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="user_projects")
+    position = models.ForeignKey(
+        "positions.Position", on_delete=models.CASCADE, related_name="project_users"
+    )
+    role = models.CharField(max_length=100, choices=ROLE_CHOICES)
+    is_blocked = models.BooleanField(default=False)
+    blocked_by = models.ForeignKey(
+        "users.User", on_delete=models.SET_NULL, null=True, related_name="blocked_users"
+    )
+
+    class Meta:
+        db_table = "project_user"
