34b3c8f56ecdd81d92a93aa71cb8e50074d4adbc
Dmitriy <pilevichdima20@gmail.com>
2024-12-30 18:24:22 +0300

    The service file was removed, the logic was moved to serializers, rewritten using mixins

--- todocore/projects/api/v1/serializers.py
+++ todocore/projects/api/v1/serializers.py
@@ -1,20 +1,7 @@
+from common.models import Position
+from django.core.exceptions import ValidationError
 from projects.models import Project, ProjectUser
 from rest_framework import serializers
-
-
-class ProjectUserSerializer(serializers.Serializer):
-    user_id = serializers.UUIDField(required=True)
-    role = serializers.ChoiceField(choices=ProjectUser.ROLE_CHOICES, default="Reader")
-    position = serializers.UUIDField(required=True)
-
-    def validate(self, data):
-        if "user_id" not in data:
-            raise serializers.ValidationError({"error": "User ID is required."})
-        return data
-
-
-class ProjectUserUpdateSerializer(serializers.Serializer):
-    role = serializers.ChoiceField(choices=ProjectUser.ROLE_CHOICES, required=True)
 
 
 class ProjectCreateUpdateSerializer(serializers.ModelSerializer):
@@ -23,44 +10,26 @@ class ProjectCreateUpdateSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def create(self, validated_data):
-        try:
-            created_by = validated_data.get("created_by")
-            if not created_by:
-                raise serializers.ValidationError({"created_by": "This field is required."})
+        created_by = validated_data.get("created_by")
+        if not created_by:
+            raise ValidationError({"created_by": "This field is required."})
 
-            project = Project.objects.create(**validated_data)
-
-            ProjectUser.objects.create(project=project, user=created_by, role="Owner")
-
-            return project
-
-        except Exception as e:
-            raise serializers.ValidationError({"error": str(e)})
+        project = Project.objects.create(**validated_data)
+        return project
 
     def update(self, instance, validated_data):
-        try:
-            for attr, value in validated_data.items():
-                setattr(instance, attr, value)
-            instance.save()
-            return instance
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
 
-        except Exception as e:
-            raise serializers.ValidationError({"error": str(e)})
+        instance.save()
 
-
-class ProjectSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = "__all__"
+        return instance
 
 
 class ProjectPartialUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
-        exclude = [
-            "created_by",
-            "created_at",
-        ]
+        exclude = ["created_by", "created_at"]
         extra_kwargs = {
             "title": {"required": False},
             "description": {"required": False},
@@ -68,13 +37,55 @@ class ProjectPartialUpdateSerializer(serializers.ModelSerializer):
         }
 
 
-class ProjectSearchSerializer(serializers.Serializer):
-    created_by = serializers.UUIDField(required=True)
-    title = serializers.CharField(required=False)
-    status = serializers.CharField(required=False)
-
-
-class ProjectSearchSerializerOutput(serializers.ModelSerializer):
+class ProjectUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Project
-        fields = ["title", "status", "created_at"]
+        model = ProjectUser
+        fields = ["id", "project", "user", "position", "role"]
+
+    def validate(self, data):
+        position = data.get("position")
+        if position and not Position.objects.filter(id=position.id).exists():
+            raise ValidationError({"position": "Position does not exist."})
+
+        role = data.get("role", "Reader")
+        valid_roles = [role[0] for role in ProjectUser.ROLE_CHOICES]
+        if role not in valid_roles:
+            raise ValidationError(
+                {"role": f"Invalid role. Allowed roles are: {', '.join(valid_roles)}"}
+            )
+
+        return data
+
+    def create(self, validated_data):
+        project = self.context["project"]
+        user = validated_data["user"]
+        role = validated_data.get("role", "Reader")
+        position = validated_data["position"]
+
+        if ProjectUser.objects.filter(project=project, user=user, position=position).exists():
+            raise ValidationError(
+                {"user": f"User with id {user.id} already exists with the same position"}
+            )
+
+        return ProjectUser.objects.create(project=project, user=user, position=position, role=role)
+
+
+class ProjectUserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectUser
+        fields = ["user", "role"]
+
+    def validate(self, data):
+        new_role = data.get("role")
+        valid_roles = [role[0] for role in ProjectUser.ROLE_CHOICES]
+        if new_role not in valid_roles:
+            raise ValidationError(
+                {"role": f"Invalid role. Allowed roles are: {', '.join(valid_roles)}"}
+            )
+        return data
+
+    def update(self, instance, validated_data):
+        new_role = validated_data.get("role")
+        instance.role = new_role
+        instance.save()
+        return instance
--- todocore/projects/api/v1/urls.py
+++ todocore/projects/api/v1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectSearchViewSet, ProjectUserViewSet, ProjectViewSet
+from .views import ProjectUserViewSet, ProjectViewSet
 
 router = DefaultRouter()
 router.register(r"project", ProjectViewSet, basename="project")
@@ -9,27 +9,17 @@ router.register(r"project", ProjectViewSet, basename="project")
 project_user_urls = [
     path(
         "project/<uuid:project_pk>/users/",
-        ProjectUserViewSet.as_view({"post": "add_user"}),
-        name="project-add-user",
+        ProjectUserViewSet.as_view({"post": "add_user", "patch": "update_user_role"}),
+        name="project-add-or-update-user",
     ),
     path(
         "project/<uuid:project_pk>/users/<uuid:user_id>/",
-        ProjectUserViewSet.as_view({"patch": "update_user_role", "delete": "delete_user"}),
-        name="project-update-user-role-or-delete-user",
+        ProjectUserViewSet.as_view({"delete": "delete_user"}),
+        name="project-delete-user",
     ),
 ]
-
-project_search_url = [
-    path(
-        "api/v1/project/",
-        ProjectSearchViewSet.as_view({"get": "list"}),
-        name="project-search",
-    ),
-]
-
 
 urlpatterns = [
     path("", include(router.urls)),
     path("", include(project_user_urls)),
-    path("", include(project_search_url)),
 ]
